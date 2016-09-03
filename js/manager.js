c3b79fb8a148c03853af407ca18f8bb9c1a27b2e
ksami <ksami.ihide@gmail.com>
2016-09-03 23:41:56 +0800

    feature(manager, ai): generalize cases with manager, add 3rd player ai

---
+++ js/manager.js
@@ -0,0 +1,20 @@
+class Manager {
+  constructor(game, players=[]) {
+    this._game = game;
+    this.players = players;
+  }
+
+  update() {
+    this.checkOverlap();
+    this.players.forEach(player=>player.update());
+  }
+
+  checkOverlap() {
+    this._game.physics.arcade.overlap(this.players.map(player=>player.object), this.players.map(player=>player.lasers), this.onScore, null, this);
+  }
+
+  onScore(playerObject, laser) {
+    laser.kill();
+    laser.parent.owner.onScore();
+  }
+}
--- js/player.js
+++ js/player.js
@@ -3,9 +3,11 @@ class Player {
     opts.keys = opts.keys || {};
 
     // Internal
+    this._isAI = opts.isAI || false;
     this._game = game;
     this._timeNextFire = 0;
     this._scoreCount = 0;
+    this._facingUpNegator = -1;
 
     // Constants
     this.SPEED_MOVE = opts.SPEED_MOVE || 400;
@@ -14,9 +16,11 @@ class Player {
 
     // Sprite
     this.object = this._game.add.sprite(x, y, "player");
+    this.object.owner = this;
     this.object.anchor.setTo(0.5, 0.5);
     this.isFacingUp = opts.isFacingUp || false;
-    if (!this.isFacingUp){
+    if (!this.isFacingUp) {
+      this._facingUpNegator *= -1;
       this.object.angle = 180;
     }
 
@@ -25,14 +29,20 @@ class Player {
     this.object.body.collideWorldBounds = true;
     
     // Input
-    this.keys = this._game.input.keyboard.addKeys({
-      "left": opts.keys.left || Phaser.KeyCode.A,
-      "right": opts.keys.right || Phaser.KeyCode.D,
-      "fire": opts.keys.fire || Phaser.KeyCode.W
-    });
+    if (!this._isAI) {
+      this.keys = this._game.input.keyboard.addKeys({
+        "left": opts.keys.left || Phaser.KeyCode.A,
+        "right": opts.keys.right || Phaser.KeyCode.D,
+        "fire": opts.keys.fire || Phaser.KeyCode.W
+      });
+    } else {
+      this._aiTimeNextAction = 0;
+      this.AI_RATE_ACTION = opts.AI_RATE_ACTION || 100;
+    }
 
     // Lasers
     this.lasers = this._game.add.group();
+    this.lasers.owner = this;
     this.lasers.enableBody = true;
     this.lasers.physicsBodyType = Phaser.Physics.ARCADE;
     this.lasers.createMultiple(5, "laser", null, false);
@@ -45,6 +55,14 @@ class Player {
   }
 
   update() {
+    if (!this._isAI) {
+      this.handleMovement();
+    } else {
+      this.aiHandleMovement();
+    }
+  }
+
+  handleMovement() {
     //  Reset the players velocity (movement)
     this.object.body.velocity.x = 0;
 
@@ -59,6 +77,30 @@ class Player {
     }
   }
 
+  aiHandleMovement() {
+    if (this._game.time.now > this._aiTimeNextAction) {
+      this._aiTimeNextAction = this._game.time.now + this.AI_RATE_ACTION;
+
+      var rand = this._game.rnd.integerInRange(0, 100);
+      if (rand <= 25) {
+        this.moveLeft();
+      } else if(rand <= 50) {
+        this.moveRight();
+      } else if (rand <= 90) {
+        this.fire();
+      } else {
+        this.flipDirection();
+      }
+    }
+  }
+
+
+  flipDirection() {
+    this._facingUpNegator *= -1;
+    this.isFacingUp = !this.isFacingUp;
+    this.object.angle = (Math.abs(this.object.angle) >= 180) ? 0 : 180;
+  }
+
   moveLeft() {
     this.object.body.velocity.x = -this.SPEED_MOVE;
   }
@@ -71,14 +113,13 @@ class Player {
     if (this._game.time.now > this._timeNextFire && this.lasers.countDead() > 0) {
       this._timeNextFire = this._game.time.now + this.RATE_FIRE;
       var laser = this.lasers.getFirstDead();
-      laser.reset(this.object.x, this.object.y);
-      laser.body.velocity.y = this.isFacingUp ? -this.SPEED_MOVE_LASER : this.SPEED_MOVE_LASER;
+      laser.reset(this.object.x, this.object.y + (16*this._facingUpNegator));
+      laser.body.velocity.y = this.SPEED_MOVE_LASER * this._facingUpNegator;
     }
   }
 
 
-  onScore(otherPlayer, laser) {
-    laser.kill();
+  onScore() {
     this._scoreCount++;
   }
 }
